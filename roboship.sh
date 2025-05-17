815d073f5aa27405972352bc41be6e4fb34a7b0b
jagadeswari <jagadeswaribade@gmail.com>
2025-05-17 21:00:05 +0530

    updated

--- roboship.sh
+++ roboship.sh
@@ -5,6 +5,17 @@ userid=$(id -u)
 if [ $userid -ne 0 ]
 then
     echo "ERR : login with root access"
+    exit 1
 else
     echo "you are in root access"
+fi
+
+dnf install mysql -y
+
+if [ $? -eq 0 ]
+then
+    echo "MySQL installation got Success..!!!"
+else
+    echo "ERR : user not in root access"
+    exit 1
 fi
