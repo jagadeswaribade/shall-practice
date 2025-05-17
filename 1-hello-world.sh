bcd7f9862209072a499312c9fac11752a11ca790
jagadeswari <jagadeswaribade@gmail.com>
2025-05-17 16:08:00 +0530

    updated

--- 1-hello-world.sh
+++ 1-hello-world.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-kid1=$1
-kid2=$2
+echo "enter ur pin:"
 
-echo "$kid1 : i am angry on $kid2"
-echo "$kid2 : y, what happened with me $kid1"
-echo "$kid1 : i am angry on $kid2"
-echo "$kid1 : y, what happened with me $kid2"
+read PIN
+
+echo "ur pin num is"
