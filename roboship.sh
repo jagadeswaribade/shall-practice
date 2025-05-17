4fc8f34d9576d20298470a599719f37de6776b8d
jagadeswari <jagadeswaribade@gmail.com>
2025-05-17 19:11:18 +0530

    updated

--- roboship.sh
+++ roboship.sh
@@ -7,5 +7,6 @@ echo "home directory : $HOME"
 echo "User directory : $USER"
 echo "present working directory : $PWD"
 echo "process id : $$"
+sleep 10 &
 echo "process running in background : $!"
 echo "exit status : $?"
