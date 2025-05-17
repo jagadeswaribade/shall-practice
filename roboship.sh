c1591ac49a716b5eb13002b316de69269e680dba
jagadeswari <jagadeswaribade@gmail.com>
2025-05-17 19:31:19 +0530

    updated

--- roboship.sh
+++ roboship.sh
@@ -4,7 +4,7 @@ num=$2
 
 if [ $num -gt 10 ]
 then 
-echo "The number $num is greater than 10"
+    echo "The number $num is greater than 10"
 else 
-echo "The number $num is less than 10" 
+    echo "The number $num is less than 10" 
 fi
