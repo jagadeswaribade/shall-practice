b428472eade2365feed4be79e3a610c81153c94e
jagadeswari <jagadeswaribade@gmail.com>
2025-05-17 19:10:06 +0530

    updated

--- roboship.sh
+++ roboship.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-fruits=("mango" "apple" "watermelon")
-
-echo "my favourite fruit : ${fruits[@]}"
+echo "all variables : $@"
+echo "number of variables : $#"
+echo "file name : $0"
+echo "home directory : $HOME"
+echo "User directory : $USER"
+echo "present working directory : $PWD"
+echo "process id : $$"
+echo "process running in background : $!"
+echo "exit status : $?"
