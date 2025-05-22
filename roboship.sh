e80e69d368e68d1fdfb3cad322b3174e2dd98bdc
jagadeswari <jagadeswaribade@gmail.com>
2025-05-22 16:13:52 +0530

    created 11 instances

--- roboship.sh
+++ roboship.sh
@@ -1,14 +1,41 @@
 #!/bin/bash
 
-userid=$(id -u)
+AMI_ID="ami-09c813fb71547fc4f"
+SG_ID="sg-0705f6396c1dd7844" # replace with your SG ID
+INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "frontend")
+ZONE_ID="Z07126283F8YDXO74P48A" # replace with your ZONE ID
+DOMAIN_NAME="jaggu.shop" # replace with your domain
 
-echo "$userid"
+#for instance in ${INSTANCES[@]}
+for instance in $@
+do
+    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-0705f6396c1dd7844 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
+    if [ $instance != "frontend" ]
+    then
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
+        RECORD_NAME="$instance.$DOMAIN_NAME"
+    else
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
+        RECORD_NAME="$DOMAIN_NAME"
+    fi
+    echo "$instance IP address: $IP"
 
-if [ $userid -ne 0 ]
-then
-    echo "ERR : login with root access"
-    exit 1
-else
-    echo "you are in root access"
-fi
-
+    aws route53 change-resource-record-sets \
+    --hosted-zone-id $ZONE_ID \
+    --change-batch '
+    {
+        "Comment": "Creating or Updating a record set for cognito endpoint"
+        ,"Changes": [{
+        "Action"              : "UPSERT"
+        ,"ResourceRecordSet"  : {
+            "Name"              : "'$RECORD_NAME'"
+            ,"Type"             : "A"
+            ,"TTL"              : 1
+            ,"ResourceRecords"  : [{
+                "Value"         : "'$IP'"
+            }]
+        }
+        }]
+    }'
+    
+done
