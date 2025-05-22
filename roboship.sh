ae301054a861bbd4c58c3eeb2c1c59040706d955
jagadeswari <jagadeswaribade@gmail.com>
2025-05-22 16:56:11 +0530

    created 11 instances

--- roboship.sh
+++ roboship.sh
@@ -6,8 +6,8 @@ INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipp
 ZONE_ID="Z07126283F8YDXO74P48A" # replace with your ZONE ID
 DOMAIN_NAME="jaggu.shop" # replace with your domain
 
-#for instance in ${INSTANCES[@]}
-for instance in $@
+for instance in ${INSTANCES[@]}
+#for instance in $@
 do
     INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-0705f6396c1dd7844 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
     if [ $instance != "frontend" ]
