7e5c16324b43875362fd7f47c8594423e4c767de
jagadeswari <jagadeswaribade@gmail.com>
2025-05-24 23:15:41 +0530

    deleting logs

--- files.sh
+++ files.sh
@@ -1,43 +1,44 @@
 #!/bin/bash
-USERID=(id -u)
+
+USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
-
-LOGS_FOLDER="/var/log/shall-practice"
-SOURCE_DIR="/home/ec2-user/app-log"
+LOGS_FOLDER="/var/log/shellscript-logs"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+SOURCE_DIR=/home/ec2-user/app-logs
 
 mkdir -p $LOGS_FOLDER
 
 if [ $USERID -ne 0 ]
 then
-    echo -e "$R ERROR : $N ur not user,login with root access" | tee -a $LOG_FILE
-    exit 1
+    echo -e "$R ERROR:: Please run this script with root access $N" | tee -a $LOG_FILE
+    exit 1 #give other than 0 upto 127
 else
-
-    echo -e "$Y SUCCESS : $N ur the root user" | tee -a $LOG_FILE
+    echo "You are running with root access" | tee -a $LOG_FILE
 fi
 
+# validate functions takes input as exit status, what command they tried to install
 VALIDATE(){
     if [ $1 -eq 0 ]
-    then  
-        echo -e "$2 is $Y success... " | tee -a $LOG_FILE
+    then
+        echo -e "$2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
     else
-        echo -e "$2 is $R ERROR...." | tee -a $LOG_FILE
+        echo -e "$2 is ... $R FAILURE $N" | tee -a $LOG_FILE
         exit 1
+    fi
 }
 
-
-echo "Script started at $(date)" | tee -a $LOG_FILE
+echo "Script started executing at $(date)" | tee -a $LOG_FILE
 
 FILES_TO_DELETE=$(find $SOURCE_DIR -name "*.log" -mtime +14)
 
-while IFS = read -r filepath
+while IFS= read -r filepath
 do
-    echo "deleting the files : $filepath" | tee -a $LOG_FILE
+    echo "Deleting file: $filepath" | tee -a $LOG_FILE
     rm -rf $filepath
 done <<< $FILES_TO_DELETE
-echo "script successfully executed"
+
+echo "Script executed successfully"
