98dbe0f0f2efd382fa9327b7800a4a9c07ac29d7
jagadeswari <jagadeswaribade@gmail.com>
2025-05-25 15:58:15 +0530

    all instances

--- mongodb.sh
+++ mongodb.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 
-USERID=(id -u)
+USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
-
 LOGS_FOLDER="/var/log/roboshop-logs"
-SCRIPT_NAME=$(echo $0 | cat -d "." -f1)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
-SOURCE_DIR="$PWD"
 
 mkdir -p $LOGS_FOLDER
-
 echo "Script started executing at: $(date)" | tee -a $LOG_FILE
 
 # check the user has root priveleges or not
